53afa1b11cb7c0afb0f43b90ce775a7981d87abf
Yiru Lan <a78945612385238@gmail.com>
2024-03-16 19:14:57 +0800

    Update

---
+++ 11.arrays/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$foods = array("apple", "orange", "banana", "coconut");
+
+// echo "{$foods[0]}<br>";
+// echo "{$foods[1]}<br>";
+// echo "{$foods[2]}<br>";
+// echo "{$foods[3]}<br>";
+
+// $foods[0] = "pineapple";
+// array_push($foods, "pineapple", "kiwi");
+// array_pop($foods);
+// array_shift($foods);
+// $foods = array_reverse($foods);
+
+// foreach($foods as $food){
+//   echo "{$food}<br>";
+// }
+
+echo count($foods);
+
+// for ($i = 0; $i < count($foods); $i++) {
+//   echo "{$foods[$i]}<br>";
+// }
+
+// foreach ($foods as $food) {
+//   echo "{$food}<br>";
+// }
