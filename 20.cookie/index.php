869954db6fb5dc8a7a11f51524b3d1fd9504075e
Yiru Lan <a78945612385238@gmail.com>
2024-03-18 13:30:49 +0800

    Update

---
+++ 20.cookie/index.php
@@ -0,0 +1,19 @@
+<?php
+
+// setcookie("fav_food", "pizza", time() + 86400 * 2, "/");
+// setcookie("fav_drink", "coffee", time() + 86400 * 3, "/");
+// setcookie("fav_dessert", "ice cream", time() + 86400 * 4, "/");
+
+// setcookie("fav_food", "pizza", time() - 0, "/");
+// setcookie("fav_drink", "coffee", time() - 0, "/");
+// setcookie("fav_dessert", "ice cream", time() - 0, "/");
+
+// foreach ($_COOKIE as $key => $value) {
+//   echo "$key = $value <br>";
+// }
+
+// if (isset($_COOKIE["fav_food"])) {
+//   echo "BUY SOME {$_COOKIE['fav_food']} !!!";
+// } else {
+//   echo "I don't know your favorite food";
+// }
